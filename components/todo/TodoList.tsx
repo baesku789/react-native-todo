64d11e18700f64dcbe7e442efcdf35b30fda4154
yechan <baesku789@gmail.com>
2022-12-12 15:05:29 +0900

    feat: - TodoList key 적용

--- components/todo/TodoList.tsx
+++ components/todo/TodoList.tsx
@@ -19,6 +19,7 @@ const TodoList = ({todos}: TodoListProps) => {
       contentContainerStyle={styles.container}
       data={todos}
       renderItem={renderItem}
+      keyExtractor={item => item.id}
     />
   );
 };
